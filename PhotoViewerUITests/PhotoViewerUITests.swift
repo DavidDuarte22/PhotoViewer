494d823f7d13fb218d4e275d1de4c111dcd87560
David Duarte <davidjulian.duarte.contractor@bbva.com>
2022-01-08 17:59:35 +0100

    adding more tests

--- PhotoViewerUITests/PhotoViewerUITests.swift
+++ PhotoViewerUITests/PhotoViewerUITests.swift
@@ -44,13 +44,4 @@ class PhotoViewerUITests: XCTestCase {
         app.tables/*@START_MENU_TOKEN@*/.buttons["love"]/*[[".cells.buttons[\"love\"]",".buttons[\"love\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
            
     }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
 }
