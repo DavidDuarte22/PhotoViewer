8f43b0d1876b00f80968ce2e5b205785289d86d9
David Duarte <davidjulian.duarte.contractor@bbva.com>
2022-01-08 17:51:03 +0100

    adding more tests

--- PhotoViewerUITests/PhotoViewerUITests.swift
+++ PhotoViewerUITests/PhotoViewerUITests.swift
@@ -24,26 +24,27 @@ class PhotoViewerUITests: XCTestCase {
 
     func testExample() throws {
         // UI tests must launch the application that they test.
+        
         let app = XCUIApplication()
         app.activate()
-        app.collectionViews.children(matching: .cell).element(boundBy: 5).tap()
-        let closeButton = app.buttons["Close"]
-        //TODO: Improve the wait. Should use a mocked UIImage instead.
+        let tabBar = app.tabBars["Tab Bar"]
+        let bookmarkButton = tabBar.buttons["bookmark"]
+        bookmarkButton.tap()
+        tabBar.buttons["home"].tap()
         
-        XCTAssertTrue(closeButton.waitForExistence(timeout: 12))
-        app/*@START_MENU_TOKEN@*/.buttons["love"]/*[[".cells.buttons[\"love\"]",".buttons[\"love\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        closeButton.tap()
-        app.tabBars["Tab Bar"].buttons["bookmark"].tap()
+        let collectionViewsQuery = app.collectionViews
+        let cell = collectionViewsQuery.children(matching: .cell).element(boundBy: 6)
+        cell.swipeUp()
+        cell.swipeUp()
+        collectionViewsQuery.children(matching: .cell).element(boundBy: 3).tap()
+        XCTAssertTrue(app.buttons["love"].waitForExistence(timeout: 12))
+        app.buttons["love"].tap()
+        app.buttons["Close"].tap()
+        bookmarkButton.tap()
         app.tables/*@START_MENU_TOKEN@*/.buttons["love"]/*[[".cells.buttons[\"love\"]",".buttons[\"love\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-                
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+           
     }
-    
-    func testErrorExample() throws {
-        
-    }
-    
+
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
             // This measures how long it takes to launch your application.
